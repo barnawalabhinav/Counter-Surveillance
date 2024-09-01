967f434bb1962c2d2302ee255121d6a92bf61d47
barnawalabhinav <abhinav.barnawal@iitd.ac.in>
2024-09-01 13:54:48 +0530

    Implemented Precision Recall

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!bin/bash/
 
 g++ -O3 src/main.cpp -o src/main.o
-src/main.o --n 5
+src/main.o --n 100 --m 10 --k 5 --p 1.0 --q 0.0
 rm src/main.o
--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #include <random>
 #include <algorithm>
 
-
 void display_help(const std::string &program_name)
 {
     std::cout << "Usage: " << program_name << " [options]\n"
@@ -17,12 +16,53 @@ void display_help(const std::string &program_name)
               << "  --m <int>       Specify the Number of Students to pick for rollcall\n"
               << "  --k <int>       Specify the Minimum Number of Votes required to be present\n"
               << "  --a <int>       Specify the Maximum Number of students that each student can ask for vote\n"
+              << "  --b <int>       Specify the Penalty for voting for absent student\n"
               << "  --p <float>     Specify the Probability that a student is marked present if he/she is present\n"
               << "  --q <float>     Specify the Probability that a student is marked present if he/she is absent\n"
               << "  --r <float>     Specify the Probability that a student is present in the class\n"
               << std::endl;
 }
 
+double calc_precision(std::vector<bool> &gold, std::vector<bool> &pred)
+{
+    int tp = 0, fp = 0;
+    for (int i = 0; i < gold.size(); i++)
+    {
+        if (gold[i] && pred[i])
+            tp++;
+        else if (pred[i] && !gold[i])
+            fp++;
+    }
+    return (double)tp / (tp + fp);
+}
+
+double calc_recall(std::vector<bool> &gold, std::vector<bool> &pred)
+{
+    int tp = 0, fn = 0;
+    for (int i = 0; i < gold.size(); i++)
+    {
+        if (gold[i] && pred[i])
+            tp++;
+        else if (gold[i] && !pred[i])
+            fn++;
+    }
+    return (double)tp / (tp + fn);
+}
+
+double calc_f1_score(std::vector<bool> &gold, std::vector<bool> &pred)
+{
+    int tp = 0, fn = 0, fp = 0;
+    for (int i = 0; i < gold.size(); i++)
+    {
+        if (gold[i] && pred[i])
+            tp++;
+        else if (gold[i] && !pred[i])
+            fn++;
+        else if (!gold[i] && pred[i])
+            fp++;
+    }
+    return (double)(2 * tp) / (2 * tp + fn + fp);
+}
 
 /*
 Simulate the decentralized attendance system in a class
@@ -35,8 +75,10 @@ a: Maximum Number of students that each student can ask for vote
 p: Probability that a student is marked present if he/she is present
 q: Probability that a student is marked present if he/she is absent
 r: Probability that a student is present in the class
+seed: Random Number Generator Seed
+b: b for voting for absent student
 */
-inline void simulate(int n, int m, int k, int a, float p, float q, float r, int seed = -1, int penalty = 5)
+inline double simulate(int n, int m, int k, int a, float p, float q, float r, int seed = -1, int b = 5)
 {
     std::bernoulli_distribution vote_present_dist(p);
     std::bernoulli_distribution vote_absent_dist(q);
@@ -49,7 +91,7 @@ inline void simulate(int n, int m, int k, int a, float p, float q, float r, int 
         generator = std::mt19937(rd());
     }
 
-    bool present_status[n];
+    std::vector<bool> present_status(n);
     std::vector<int> voters[n];
     std::vector<int> students(n);
     for (int i = 0; i < n; ++i)
@@ -66,11 +108,15 @@ inline void simulate(int n, int m, int k, int a, float p, float q, float r, int 
     for (int i = 0; i < n; i++)
     {
         std::shuffle(students.begin(), students.end(), generator);
-        int a_i = rand() % (a + 1);
-        for (int j = 0; j < a + 1; j++)
+        // int a_i = rand() % (a + 1);
+        int a_i = a;
+        for (int j = 0; j < a_i; j++)
         {
-            if (students[j] != i)
+            if (students[j] == i)
+            {
+                a_i++;
                 continue;
+            }
 
             if (present_status[i])
             {
@@ -95,25 +141,42 @@ inline void simulate(int n, int m, int k, int a, float p, float q, float r, int 
         {
             attendance_criteria[student] = n + 1;
             for (int voter : voters[student])
-                attendance_criteria[voter] += penalty;
+                attendance_criteria[voter] += b;
         }
     }
+
+    // Check if each student is present or not
+    std::vector<bool> marked_present(n);
+    for (int i = 0; i < n; i++)
+    {
+        // std::cout << "Student " << i << " : " << voters[i].size() << std::endl;
+        if (voters[i].size() >= attendance_criteria[i])
+            marked_present[i] = true;
+        else
+            marked_present[i] = false;
+    }
+
+    std::cout << "Precision: " << calc_precision(present_status, marked_present) << std::endl;
+    std::cout << "Recall: " << calc_recall(present_status, marked_present) << std::endl;
+
+    double f1_score = calc_f1_score(present_status, marked_present);
+    return f1_score;
 }
 
 int main(int argc, char *argv[])
 {
     // ################## PARSING CMD LINE ARGS ##################
-    int num_threads = 1;
-    int seed = 0;
-    int sim = 1000;
-    int n = 100;
-    int m = 10;
-    int k = 5;
-    int a = 5;
-    int penalty = 5;
-    float p = 0.9;
-    float q = 0.1;
-    float r = 0.6;
+    int num_threads = 1; // Parallel Threads Count
+    int seed = -1;       // Random Number Generator Seed
+    int sim = 1000;      // Simmulations
+    int n = 100;         // Strength
+    int m = 10;          // Roll Call Count
+    int k = 5;           // Attendance Criteria
+    int a = 5;           // Vote Limit
+    int b = 5;           // Penalty for voting for absent student
+    float p = 0.9;       // Vote Present Prob
+    float q = 0.1;       // Vote Absent Probt
+    float r = 0.6;       // Present Prob
 
     for (int i = 1; i < argc; ++i)
     {
@@ -138,6 +201,8 @@ int main(int argc, char *argv[])
             k = std::stoi(argv[++i]);
         else if (arg == "--a" && i + 1 < argc)
             a = std::stoi(argv[++i]);
+        else if (arg == "--b" && i + 1 < argc)
+            b = std::stoi(argv[++i]);
         else if (arg == "--p" && i + 1 < argc)
             p = std::stof(argv[++i]);
         else if (arg == "--q" && i + 1 < argc)
@@ -146,7 +211,7 @@ int main(int argc, char *argv[])
             r = std::stof(argv[++i]);
         else
         {
-            std::cerr << "Unknown option: " << arg << std::endl;
+            std::cerr << "Error: Unknown option: " << arg << std::endl;
             display_help(argv[0]);
             return 1;
         }
@@ -155,52 +220,57 @@ int main(int argc, char *argv[])
     // ################## VALIDATING INPUTS ##################
     if (n < 1)
     {
-        std::cerr << "Number of Students in the Class should be greater than 0" << std::endl;
+        std::cerr << "Error: Number of Students in the Class should be greater than 0" << std::endl;
         return 1;
     }
     if (m < 0)
     {
-        std::cerr << "Number of Students to pick for rollcall should be greater than or equal to 0" << std::endl;
+        std::cerr << "Error: Number of Students to pick for rollcall should be greater than or equal to 0" << std::endl;
         return 1;
     }
     if (k < 0)
     {
-        std::cerr << "Minimum Number of Votes required to be present should be greater than or equal to 0" << std::endl;
+        std::cerr << "Error: Minimum Number of Votes required to be present should be greater than or equal to 0" << std::endl;
         return 1;
     }
     if (a < 0)
     {
-        std::cerr << "Maximum Number of students that each student can ask for vote should be greater than or equal to 0" << std::endl;
+        std::cerr << "Error: Maximum Number of students that each student can ask for vote should be greater than or equal to 0" << std::endl;
+        return 1;
+    }
+    if (b < 0)
+    {
+        std::cerr << "Error: Penalty for voting for absent student should be greater than or equal to 0" << std::endl;
         return 1;
     }
     if (p < 0 || p > 1)
     {
-        std::cerr << "Probability that a student is marked present if he/she is present should be between 0 and 1" << std::endl;
+        std::cerr << "Error: Probability that a student is marked present if he/she is present should be between 0 and 1" << std::endl;
         return 1;
     }
     if (q < 0 || q > 1)
     {
-        std::cerr << "Probability that a student is marked present if he/she is absent should be between 0 and 1" << std::endl;
+        std::cerr << "Error: Probability that a student is marked present if he/she is absent should be between 0 and 1" << std::endl;
         return 1;
     }
     if (r < 0 || r > 1)
     {
-        std::cerr << "Probability that a student is present in the class should be between 0 and 1" << std::endl;
+        std::cerr << "Error: Probability that a student is present in the class should be between 0 and 1" << std::endl;
         return 1;
     }
     if (m > n)
     {
-        std::cerr << "Number of students to pick for rollcall should be less than or equal to the number of students in the class" << std::endl;
+        std::cerr << "Error: Number of students to pick for rollcall should be less than or equal to the number of students in the class" << std::endl;
         return 1;
     }
     if (a > n - 1)
     {
-        std::cerr << "Maximum Number of students that each student can ask for vote should be less than the number of students in the class" << std::endl;
+        std::cerr << "Error: Maximum Number of students that each student can ask for vote should be less than the number of students in the class" << std::endl;
         return 1;
     }
     if (k > n - 1)
     {
-        std::cerr << "Minimum Number of Votes required to be present should be less than the number of students in the class" << std::endl;
+        std::cerr << "Error: Minimum Number of Votes required to be present should be less than the number of students in the class" << std::endl;
         return 1;
     }
 
@@ -213,12 +283,14 @@ int main(int argc, char *argv[])
     std::cout << "Number of Students to pick for rollcall ---------------------------- m: " << m << std::endl;
     std::cout << "Minimum Number of Votes required to be present --------------------- k: " << k << std::endl;
     std::cout << "Maximum Number of students that each student can ask for vote ------ a: " << a << std::endl;
+    std::cout << "Penalty for voting a absent student -------------------------------- b: " << a << std::endl;
     std::cout << "Probability that a student is marked present if he/she is present -- p: " << p << std::endl;
     std::cout << "Probability that a student is marked present if he/she is absent --- q: " << q << std::endl;
     std::cout << "Probability that a student is present in the class ----------------- r: " << r << std::endl;
     std::cout << "############################################################################ " << std::endl;
 
-    simulate(n, m, k, a, p, q, r);
+    double f1_score = simulate(n, m, k, a, p, q, r);
+    std::cout << "F1 Score: " << f1_score << std::endl;
 
     return 0;
 }
